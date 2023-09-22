596d843ef40df7810ab68de06347f2e5af475136
Daniel Proctor <dan@danielproctor.dev>
2023-09-22 19:17:13 +0100

    update terminal config

--- powershell/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ powershell/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\paradox.omp.json" | Invoke-Expression
+# From https://learn.microsoft.com/en-us/windows/terminal/tutorials/custom-prompt-setup
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\catppuccin.omp.json" | Invoke-Expression
 
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
@@ -13,3 +14,30 @@ if (Test-Path($ChocolateyProfile)) {
 # https://github.com/ralish/PSDotFiles
 $DotFilesAutodetect = 'true'
 $DotFilesPath = "$HOME\dot-files-win"
+
+
+# From https://learn.microsoft.com/en-us/powershell/scripting/learn/shell/creating-profiles?view=powershell-7.3
+## Set PSReadLine options and keybindings
+$PSROptions = @{
+    ContinuationPrompt = '  '
+    PredictionViewStyle = 'ListView'
+    Colors             = @{
+        Operator         = $PSStyle.Foreground.Magenta
+        Parameter        = $PSStyle.Foreground.Magenta
+        Selection        = $PSStyle.Background.BrightBlack
+        InLinePrediction = $PSStyle.Foreground.BrightYellow + $PSStyle.Background.BrightBlack
+    }
+}
+Set-PSReadLineOption @PSROptions
+Set-PSReadLineKeyHandler -Chord 'Ctrl+f' -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord 'Enter' -Function ValidateAndAcceptLine
+
+## Add argument completer for the dotnet CLI tool
+$scriptblock = {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    dotnet complete --position $cursorPosition $commandAst.ToString() |
+        ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock $scriptblock
