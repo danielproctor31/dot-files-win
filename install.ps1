3e963975872239acbae2bed9cfcc945c2283becb
Daniel Proctor <dan@danielproctor.dev>
2023-09-29 21:47:53 +0100

    add nvim

--- install.ps1
+++ install.ps1
@@ -6,6 +6,13 @@ scoop bucket add nerd-fonts
 scoop install CascadiaCode-NF
 scoop install CascadiaCode-NF-Mono
 
+Write-Output "Installing Neovim"
+winget install Neovim.Neovim
+
+Write-Output "Installing vim plug"
+Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    New-Item "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+
 Write-Output "linking dot-files"
 
 $env:DotFilesAutodetect = 'true'
