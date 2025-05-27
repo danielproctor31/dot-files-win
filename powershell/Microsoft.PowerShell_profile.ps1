206a77174f1a1fc4d1c4c718fb46b0b970a7ce0f
Daniel Proctor <dan@danielproctor.dev>
2025-05-28 00:37:15 +0100

    use POSH_THEMES_PATH

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,5 @@
 # From https://learn.microsoft.com/en-us/windows/terminal/tutorials/custom-prompt-setup
-oh-my-posh init pwsh --config "$env:USERPROFILE\omp.json" | Invoke-Expression
-
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\jandedobbeleer.omp.json" | Invoke-Expression
 
 # From https://learn.microsoft.com/en-us/powershell/scripting/learn/shell/creating-profiles?view=powershell-7.3
 ## Set PSReadLine options and keybindings
